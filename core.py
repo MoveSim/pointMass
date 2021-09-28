04686718e5763db119069937f660a91cedebe798
alexanderoonk26@gmail.com <AlamShit@users.noreply.github.com>
2021-09-28 21:09:57 +0200

    bounce Baby

--- core.py
+++ core.py
@@ -7,30 +7,34 @@ x0 = 10
 v0 = 0
 mass = 5
 g = -9.81
-t_max = 10
+t_max = 20
 delta_t = 0.001
 precision = 1000
+bounce_coeff = 0.8
 
-## core calculations
+# core calculations
 x = [x0]
 v = [v0]
 t_array = range(0, int(t_max*precision), int(delta_t*precision))
 
 for i in range(1, len(t_array)):
+    if x[-1] <= 0:
+        v[-1] *=-1 * bounce_coeff
     v.append(v[i-1] + g*delta_t)
     x.append(max(0, x[i-1] + v[i-1]*delta_t + g*math.pow(delta_t, 2)))
 
+# save the data
 data = {
     "x": x,
     "v": v,
-    "t": range(0, t_max, delta_t)
+    "time(s)": list(x/precision for x in range(0, t_max*precision))
 }
-
 data = pd.DataFrame(data)
-import pdb; pdb.set_trace()
-x.reset_index(x.index/precision)
+data = data.set_index(data["time(s)"])
+data = data.drop("time(s)", axis=1)
 
-x.plot()
+data.plot()
 plt.show()
+
 
 import pdb; pdb.set_trace()
