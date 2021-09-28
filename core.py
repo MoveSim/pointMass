775e92857cf0bb72e055ce5d3ef0639ad9aad5e9
alexanderoonk26@gmail.com <AlamShit@users.noreply.github.com>
2021-09-28 19:04:15 +0200

    changes

--- core.py
+++ core.py
@@ -7,9 +7,9 @@ x0 = 10
 v0 = 0
 mass = 5
 g = -9.81
-t_max = 1
-delta_t = 0.01
-precision = 100
+t_max = 10
+delta_t = 0.001
+precision = 1000
 
 ## core calculations
 x = [x0]
@@ -18,7 +18,12 @@ t_array = range(0, int(t_max*precision), int(delta_t*precision))
 
 for i in range(1, len(t_array)):
     v.append(v[i-1] + g*delta_t)
-    x.append(x[i-1] + v[i-1]*delta_t + g*math.pow(delta_t, 2))
+    x.append(max(0, x[i-1] + v[i-1]*delta_t + g*math.pow(delta_t, 2)))
 
+x = pd.DataFrame(x)
+x.reset_index(x.index/precision)
+
+x.plot()
+plt.show()
 
 import pdb; pdb.set_trace()
