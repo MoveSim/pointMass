6cd0c9ecb5ab7f8f95d21619c240e462d9e6a12c
alexanderoonk26@gmail.com <AlamShit@users.noreply.github.com>
2021-09-29 08:30:46 +0200

    added requested changes

--- core.py
+++ core.py
@@ -3,29 +3,29 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 ## Variables
-x0 = 10
+y0 = 10
 v0 = 0
 mass = 5
 g = -9.81
 t_max = 20
 delta_t = 0.001
-precision = 1000
+precision = int(1/delta_t)
 bounce_coeff = 0.8
 
 # core calculations
-x = [x0]
+y = [y0]
 v = [v0]
-t_array = range(0, int(t_max*precision), int(delta_t*precision))
+t_array = range(int(t_max*precision))
 
 for i in range(1, len(t_array)):
-    if x[-1] <= 0:
-        v[-1] *=-1 * bounce_coeff
+    if y[-1] <= 0:
+        v[-1] *= -1 * bounce_coeff
     v.append(v[i-1] + g*delta_t)
-    x.append(max(0, x[i-1] + v[i-1]*delta_t + g*math.pow(delta_t, 2)))
+    y.append(max(0, y[i-1] + v[i-1]*delta_t + g*math.pow(delta_t, 2)))
 
 # save the data
 data = {
-    "x": x,
+    "y": y,
     "v": v,
     "time(s)": list(x/precision for x in range(0, t_max*precision))
 }
